426b214065e9be6c770498a69112ad7d312651d4
mahta73 <techandcodeaddict@gmail.com>
2019-04-15 12:21:10 +0400

    mongodb is connected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import TodoList from './components/todos-list.component';
+import EditTodo from './components/edit-todo.component';
+import CreateTodo from './components/create-todo.component';
+
 import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className = 'container'>
+          <nav className = 'navbar navbar-expand-lg navbar-light bg-light'>
+            <a className = 'navbar-brand' href = 'https://codingthesmartway.com' target = '_blank'>
+              <img src = {logo} width = '30' height = '30' alt = 'CodingTheSmartWay.com'/>
+            </a>
+            <Link to ='/' className = 'navbar-brand'>MERN-Stack Todo App</Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+            </nav>
+          </div>
+        <Route path = '/' exact component = {TodoList} />
+        <Route path = '/edit/:id' component = {EditTodo} />
+        <Route path = '/create' component = {CreateTodo} />
+      </Router>
     );
   }
 }
